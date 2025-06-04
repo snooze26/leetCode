db4e4b8a39077a8938251f85381298e55d127f80
snooze26 <langdon.douglas26@gmail.com>
2025-06-04 12:23:41 -0500

    did some practice on lunch

--- arrays/removeDuplicates2.js
+++ arrays/removeDuplicates2.js
@@ -1,23 +1,37 @@
-/**
+//typed 6 times as of 6/4/25 
+/** 
  * @param {number[]} nums
  * @return {number}
  */
-var removeDuplicates = function(nums) {
-    if (nums.length <= 2 )return nums.length; // check for 2 because no need to iterate through first two values 
+var removeDuplicates2 = function(nums) {
+    if(nums.length <= 2) return 2; 
 
-    let i = 2 // no need to iterate through first two values since they are valid;
+    let i = 2;
 
-    for ( let j = 2; j < nums.length; j++) {  // loop through nums starting at 
-        if(nums[j] !== nums[i - 2]) { // check 2 elements previous to see if matching
-            nums[i] = nums[j]; // set i as unique value  
-            console.log(nums); // 
-            i++ // increment i 
+    for ( let j = 2; j < nums.length; j++) { 
+        if(nums[j] !== nums[i-2]) { 
+            nums[i] = nums[j]; 
+            i++
         }
     }
-    return i; // return i 
+    return i; 
+    // return increment amount 
+    
+    // if (nums.length <= 2 )return nums.length; // check for 2 because no need to iterate through first two values 
+
+    // let i = 2 // no need to iterate through first two values since they are valid;
+
+    // for ( let j = 2; j < nums.length; j++) {  // loop through nums starting at 
+    //     if(nums[j] !== nums[i - 2]) { // check 2 elements previous to see if matching
+    //         nums[i] = nums[j]; // set i as unique value  
+    //         console.log(nums); // 
+    //         i++ // increment i 
+    //     }
+    // }
+    // return i; // return i 
 };
 
 const practiceArray = [0,0,1,1,1,1,2,3,3];
-const testResults = removeDuplicates(practiceArray); 
+const testResults = removeDuplicates2(practiceArray); 
 
 console.log(testResults); 
