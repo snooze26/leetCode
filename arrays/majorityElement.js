018665e0ae43e7f0b60f7d5eee77f6dff322d8ea
Langdon Douglas <langdon.douglas26@gmail.com>
2025-07-03 07:13:17 -0500

    feat: majorityElement

---
+++ arrays/majorityElement.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+
+    //base check 
+    if (!nums) return null; 
+    //
+     // tracks score  
+     // current candidate for maj elem, null because nothing is chosen 
+
+    // loop through
+        
+        // if count drops to 0, choose new candidate
+    // if count drops to 0 
+    // set candidate to num selecting a new candindate 
+    // if the number is the candidate then 
+    // add to the majority counter 
+    
+    // decrement the majoirty counter 
+    //return candidate 
+
